22df33d3e8e252bf446274a072c12503e3ab93f3
shanedbutler <shanedbutler@gmail.com>
2022-11-14 10:28:40 -0500

    Create new post form component and add to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { PostForm } from "./components/PostForm";
 import PostList from "./components/PostList";
 
 function App() {
   return (
     <div className="App">
+        <PostForm />
         <PostList />
     </div>
   );
---
+++ client/src/components/PostForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react"
+import { addPost } from "../modules/PostManager";
+
+export const PostForm = () => {
+
+    const [title, setTitle] = useState("");
+    const [caption, setCaption] = useState("");
+    const [url, setUrl] = useState("");
+
+    const post = {
+        title: title,
+        caption: caption,
+        dateCreated: new Date(),
+        imageUrl: url,
+        userProfileId: 1
+    };
+
+    const handleClick = () => {
+        addPost(post);
+    }
+    
+    return (
+    <>
+        <h4>New Post</h4>
+        <label htmlFor="title">Title:</label>
+        <input property="title" onChange={e => setTitle(e.target.value)} /><br/>
+        <label htmlFor="caption">Caption:</label>
+        <input property="caption" onChange={e => setCaption(e.target.value)} /><br/>
+        <label htmlFor="url">URL:</label>
+        <input property="url" onChange={e => setUrl(e.target.value)} /><br/>
+        <button onClick={handleClick}>Submit</button>
+    </>
+    );
+};
