c263a9a9847adcab029658f73d2463008ff7bb12
shanedbutler <shanedbutler@gmail.com>
2022-11-15 13:44:17 -0500

    Removed un-used import

--- client/src/components/PostForm.js
+++ client/src/components/PostForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { addPost } from "../modules/PostManager";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 export const PostForm = () => {
