7246ac8ec95cd28967e4e02d49259fb2cd6a59dd
shanedbutler <shanedbutler@gmail.com>
2022-11-15 13:46:23 -0500

    Refactor Post to display link to details page

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardImg, CardBody } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Post = ({ post }) => {
   return (
@@ -7,11 +8,10 @@ const Post = ({ post }) => {
       <p className="text-left px-2">Posted by: {post.userProfile.name}</p>
       <CardImg top src={post.imageUrl} alt={post.title} />
       <CardBody>
-        <p>
+        <Link to={`/posts/${post.id}`}>
           <strong>{post.title}</strong>
-        </p>
+        </Link>
         <p>{post.caption}</p>
-        {post.comments.map(comment => <li key={comment.id}>{comment.message}</li>)}
       </CardBody>
     </Card>
   );
