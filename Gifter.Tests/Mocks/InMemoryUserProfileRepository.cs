c724d5408088daef97bf24e9c0ebb51101d1909b
shanedbutler <shanedbutler@gmail.com>
2022-11-28 11:19:18 -0500

    Add in-memory repo and add unit test for user profile controller

---
+++ Gifter.Tests/Mocks/InMemoryUserProfileRepository.cs
@@ -0,0 +1,80 @@
+﻿using Gifter.Models;
+using Gifter.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Gifter.Tests.Mocks
+{
+    internal class InMemoryUserProfileRepository : IUserProfileRepository
+    {
+        private readonly List<UserProfile> _data;
+
+        public List<UserProfile> InternalData
+        {
+            get
+            {
+                return _data;
+            }
+        }
+
+        public InMemoryUserProfileRepository(List<UserProfile> startingData)
+        {
+            _data = startingData;
+        }
+
+        public void Add(UserProfile user)
+        {
+            var lastUser = _data.Last();
+            user.Id = lastUser.Id + 1;
+            _data.Add(user);
+        }
+
+        public void Delete(int id)
+        {
+            var userTodelete = _data.FirstOrDefault(p => p.Id == id);
+            if (userTodelete == null)
+            {
+                return;
+            }
+
+            _data.Remove(userTodelete);
+        }
+
+        public List<UserProfile> GetAll()
+        {
+            return _data;
+        }
+
+        public UserProfile GetById(int id)
+        {
+            return _data.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Update(UserProfile user)
+        {
+            var currentUser = _data.FirstOrDefault(p => p.Id == user.Id);
+            if (currentUser == null)
+            {
+                return;
+            }
+
+            currentUser.Name = user.Name;
+            currentUser.Email = user.Email;
+            currentUser.Bio = user.Bio;
+            currentUser.ImageUrl = user.ImageUrl;
+            currentUser.DateCreated = user.DateCreated;
+        }
+        public UserProfile GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public UserProfile GetByIdWithPosts (int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gifter.Tests/UserProfileControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Gifter.Controllers;
+using Gifter.Models;
+using Gifter.Tests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Gifter.Tests
+{
+    public class UserProfileControllerTests
+    {
+        [Fact]
+        public void Get_Returns_All_UserProfiles()
+        {
+            // Arrange 
+            var userCount = 20;
+            var users = CreateTestUsers(userCount);
+
+            var repo = new InMemoryUserProfileRepository(users);
+            var controller = new UserProfileController(repo);
+
+            // Act 
+            var result = controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualUsers = Assert.IsType<List<UserProfile>>(okResult.Value);
+
+            Assert.Equal(userCount, actualUsers.Count);
+            Assert.Equal(users, actualUsers);
+        }
+
+        private List<UserProfile> CreateTestUsers(int count)
+        {
+            var users = new List<UserProfile>();
+            for (var i = 1; i <= count; i++)
+            {
+                users.Add(new UserProfile()
+                {
+                    Id = i,
+                    Name = $"Name {i}",
+                    Email = $"Email {i}",
+                    Bio = $"Bio {i}",
+                    ImageUrl = $"http://post.image.url/{i}",
+                    DateCreated = DateTime.Today.AddDays(-i),
+                });
+            }
+            return users;
+        }
+    }
+}
